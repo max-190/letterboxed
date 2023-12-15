69e76e46002190a7927fa01e1a4590705b79c8ac
Max <maxruigrok190@gmail.com>
2023-12-15 22:34:07 +0100

    added back menu, unrecognized command handler

--- src/main.cc
+++ src/main.cc
@@ -29,31 +29,33 @@ int main() {
     std::string input, letters;
     LetterBoxed game;
 
-    // std::cout << "///////////////////////////////////" << std::endl
-    //           << "// 'Letterboxed' solver          //" << std::endl
-    //           << "// Made by Max-190               //" << std::endl
-    //           << "// dec 14th, 2023                //" << std::endl
-    //           << "// Compiled using g++ with c++11 //" << std::endl
-    //           << "///////////////////////////////////" << std::endl
-    //           << std::endl;
+    std::cout << "///////////////////////////////////" << std::endl
+              << "// 'Letterboxed' solver          //" << std::endl
+              << "// Made by Max-190               //" << std::endl
+              << "// dec 14th, 2023                //" << std::endl
+              << "// Compiled using g++ with c++11 //" << std::endl
+              << "///////////////////////////////////" << std::endl
+              << std::endl;
 
-    // while (input != "exit") {
-    //     try {
-    //         print_main_menu();
-    //         std::cout << "> ";
-    //         std::cin >> input;
-    //         if (input == "new") {
-    //             std::getline(std::cin, letters);
-    //             if (is_valid_input(letters)) {
-    //                 // game(letters);
-    //             } else {
-    //                 throw std::runtime_error("Input is not valid");
-    //             }
-    //         }
-    //     } catch (std::exception const &e) {
-    //         std::cerr << "Error: " << e.what() << std::endl;
-    //     }
-    // }
+    while (input != "exit") {
+        try {
+            print_main_menu();
+            std::cout << "> ";
+            std::cin >> input;
+            if (input == "new") {
+                std::getline(std::cin, letters);
+                if (is_valid_input(letters)) {
+                    // game(letters);
+                } else {
+                    throw std::runtime_error("Input is not valid");
+                }
+            } else {
+                throw std::runtime_error("Unrecognized command: " + input);
+            }
+        } catch (std::exception const &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
 
     Graph graph;
 
